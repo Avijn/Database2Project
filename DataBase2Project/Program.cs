538a5303ff2f7d44f941058a7cbc022110e8a3e2
AVijn <arjan.witharen@gmail.com>
2024-04-01 16:10:46 +0200

    Removed commented out code

--- DataBase2Project/Program.cs
+++ DataBase2Project/Program.cs
@@ -16,8 +16,6 @@ void run()
     Console.WriteLine("     Miliseconds for 10 row:      " + adoNet.GetBeer(10));
     Console.WriteLine("     Miliseconds for 100 row:     " + adoNet.GetBeer(100));
     Console.WriteLine("     Miliseconds for 1000 row:    " + adoNet.GetBeer(1000));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + adoNet.GetBeer(10000));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + adoNet.GetBeer(100000));
 
     Console.WriteLine("\n Entity framework :");
     eFService.GetBeersWithoutTimer(1);
@@ -25,8 +23,6 @@ void run()
     Console.WriteLine("     Miliseconds for 10 row:      " + eFService.GetBeers(10));
     Console.WriteLine("     Miliseconds for 100 row:     " + eFService.GetBeers(100));
     Console.WriteLine("     Miliseconds for 1000 row:    " + eFService.GetBeers(1000));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + eFService.GetBeers(10000));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + eFService.GetBeers(100000));
 
     Console.WriteLine("\n MongoDB :");
     mongoDBConn.SelectBeersMongoDB(1);
@@ -34,8 +30,6 @@ void run()
     Console.WriteLine("     Miliseconds for 10 row:      " + mongoDBConn.SelectBeersMongoDB(10));
     Console.WriteLine("     Miliseconds for 100 row:     " + mongoDBConn.SelectBeersMongoDB(100));
     Console.WriteLine("     Miliseconds for 1000 row:    " + mongoDBConn.SelectBeersMongoDB(1000));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + mongoDBConn.SelectBeersMongoDB(10000));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + mongoDBConn.SelectBeersMongoDB(100000));
 
     Console.WriteLine("\n\nCREATE :");
     Console.WriteLine("\n Ado.Net :");
@@ -43,24 +37,18 @@ void run()
     Console.WriteLine("     Miliseconds for 10 row:      " + adoNet.InsertBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + adoNet.InsertBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + adoNet.InsertBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + adoNet.InsertBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + adoNet.InsertBeers(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n Entity framework :");
     Console.WriteLine("     Miliseconds for 1 row:       " + eFService.InsertBeers(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 10 row:      " + eFService.InsertBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + eFService.InsertBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + eFService.InsertBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + eFService.InsertBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + eFService.InsertBeers(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n MongoDB :");
     Console.WriteLine("     Miliseconds for 1 row:       " + mongoDBConn.InsertIntoMongoDB(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 10 row:      " + mongoDBConn.InsertIntoMongoDB(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + mongoDBConn.InsertIntoMongoDB(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + mongoDBConn.InsertIntoMongoDB(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + mongoDBConn.InsertIntoMongoDB(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + mongoDBConn.InsertIntoMongoDB(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n\nUPDATE :");
     Console.WriteLine("\n Ado.Net :");
@@ -68,24 +56,18 @@ void run()
     Console.WriteLine("     Miliseconds for 10 row:      " + adoNet.UpdateBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + adoNet.UpdateBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + adoNet.UpdateBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + adoNet.UpdateBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + adoNet.UpdateBeers(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n Entity framework :");
     Console.WriteLine("     Miliseconds for 1 row:       " + eFService.UpdateBeers(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 10 row:      " + eFService.UpdateBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + eFService.UpdateBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + eFService.UpdateBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + eFService.UpdateBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + eFService.UpdateBeers(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n MongoDB :");
     Console.WriteLine("     Miliseconds for 1 row:       " + mongoDBConn.UpdateBeers(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 10 row:      " + mongoDBConn.UpdateBeers(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 100 row:     " + mongoDBConn.UpdateBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + mongoDBConn.UpdateBeers(beerlist.GetRange(0, 100)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + mongoDBConn.UpdateBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + mongoDBConn.UpdateBeers(beerlist.GetRange(0, 10000)));
 
 
     Console.WriteLine("\n\nDELETE :");
@@ -94,22 +76,16 @@ void run()
     Console.WriteLine("     Miliseconds for 10 row:      " + adoNet.DeleteBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + adoNet.DeleteBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + adoNet.DeleteBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + adoNet.DeleteBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + adoNet.DeleteBeers(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n Entity framework :");
     Console.WriteLine("     Miliseconds for 1 row:       " + eFService.DeleteBeers(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 10 row:      " + eFService.DeleteBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + eFService.DeleteBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + eFService.DeleteBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + eFService.DeleteBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + eFService.DeleteBeers(beerlist.GetRange(0, 100000)));
 
     Console.WriteLine("\n MongoDB :");
     Console.WriteLine("     Miliseconds for 1 row:       " + mongoDBConn.DeleteBeers(beerlist.GetRange(0, 1)));
     Console.WriteLine("     Miliseconds for 10 row:      " + mongoDBConn.DeleteBeers(beerlist.GetRange(0, 10)));
     Console.WriteLine("     Miliseconds for 100 row:     " + mongoDBConn.DeleteBeers(beerlist.GetRange(0, 100)));
     Console.WriteLine("     Miliseconds for 1000 row:    " + mongoDBConn.DeleteBeers(beerlist.GetRange(0, 1000)));
-    //Console.WriteLine("     Miliseconds for 10000 row:   " + mongoDBConn.DeleteBeers(beerlist.GetRange(0, 10000)));
-    //Console.WriteLine("     Miliseconds for 100000 row:  " + mongoDBConn.DeleteBeers(beerlist.GetRange(0, 100000)));
 }
